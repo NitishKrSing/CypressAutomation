e23db4acd1798774c82195bd7f74341848103e94
NitishKrSing <nitishs0189@gmail.com>
2022-04-08 15:52:53 +0530

    Deleted unnassary file

--- cypress/integration/TestTry/Test.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-describe('Automate the Test Module', function() {
-
-})
-it('Verify Register Page', function() {
-    cy.visit('https://www.mytheresa.com/en-de/men.html')
-
-})
